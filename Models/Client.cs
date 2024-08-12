e0df1dcb80a6e9e94833e09ae084da69581faadb
siya045 <38082918@mynwu.ac.za>
2024-08-12 16:03:49 +0200

    Add project files.

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _38082918.Models;
+
+public partial class Client
+{
+    public Guid ClientId { get; set; }
+
+    public string? ClientName { get; set; }
+
+    public string? PrimaryContactEmail { get; set; }
+
+    public DateTime? DateOnboarded { get; set; }
+}
